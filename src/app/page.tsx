5a82a9251a94100b794fb27bcc16a34968e4c825
jduarte <77544911+jdavidduart@users.noreply.github.com>
2025-07-23 16:16:01 -0500

    render users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import AddNewUser from "@/features/addNewUser/addNewUser";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/store/store";
 
 export default function Home() {
+  const listOfUsers = useSelector((state: RootState) => state.data.users);
   return (
     <div>
       <AddNewUser />
+      {listOfUsers.map((user) => (
+        <div key={user.name}>
+          <p>Name: {user.name}</p>
+          <p>Register Date: {user.registerDate}</p>
+        </div>
+      ))}
     </div>
   );
 }
